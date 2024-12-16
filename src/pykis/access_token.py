193e6e1363f0aec91e4c27abca377581cf9182a9
cosrah <cosrah@hotmail.com>
2024-12-16 21:48:03 +0900

    AccessToken에 대한 캐시 추가

--- src/pykis/access_token.py
+++ src/pykis/access_token.py
@@ -16,6 +16,8 @@ pykis의 AccessToken 클래스를 담기 위한 모듈
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import json
 from datetime import datetime, timedelta
 from typing import NamedTuple, Optional
 
@@ -25,29 +27,49 @@ class AccessToken:
     인증용 토큰 정보를 담을 클래스
     """
 
-    def __init__(self) -> None:
+    def __init__(self, cache_file: str = "access_token_cache.json") -> None:
         self.value: Optional[str] = None
         self.valid_until: Optional[datetime] = None
+        self.cache_file = cache_file
+
+        self.load_from_cache()
 
     def create(self, resp: NamedTuple) -> None:
-        """
-        Token을 생성한다.
-        """
-        self.value: str = f"Bearer {str(resp.access_token)}"
-        self.valid_until: datetime = self._valid_until(resp)
+        self.value = f"Bearer {str(resp.access_token)}"
+        self.valid_until = self._valid_until(resp)
+        self.save_to_cache()
 
     def _valid_until(self, resp: NamedTuple) -> datetime:
-        """
-        현재 시각 기준으로 Token의 유효기한을 반환한다.
-        """
         time_margin = 60
         duration = int(resp.expires_in) - time_margin
         return datetime.now() + timedelta(seconds=duration)
 
     def is_valid(self) -> bool:
-        """
-        Token이 유효한지 검사한다.
-        """
-        return self.value is not None and \
-            self.valid_until is not None and \
+        return (
+            self.value is not None and
+            self.valid_until is not None and
             datetime.now() < self.valid_until
+        )
+
+    def save_to_cache(self) -> None:
+        if self.value and self.valid_until:
+            data = {
+                "value": self.value,
+                "valid_until": self.valid_until.isoformat()
+            }
+            with open(self.cache_file, "w") as f:
+                json.dump(data, f)
+
+    def load_from_cache(self) -> None:
+        if os.path.exists(self.cache_file):
+            with open(self.cache_file, "r") as f:
+                data = json.load(f)
+                valid_until = datetime.fromisoformat(data["valid_until"])
+
+                if datetime.now() < valid_until:
+                    self.value = data["value"]
+                    self.valid_until = valid_until
+                else:
+                    # 만료된 경우 캐시 초기화
+                    self.value = None
+                    self.valid_until = None
